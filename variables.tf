2d29ca1e25ca7dbd25b19ab8136f71dd4c20ae34
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-02-21 12:17:44 -0800

    formatting the variables file

--- variables.tf
+++ variables.tf
@@ -44,7 +44,7 @@ variable "db_subnet_name" {
 }
 
 variable "auto_create_subnets" {
-  type = bool
+  type        = bool
   description = "When set to 'true', the network is created in `auto subnet mode and it will create a subnet for each region automatically across the '10.128.0.0/9' address range. When set to 'false', the network is created in `custom subnet mode` so the user can explicitly connect subnetwork resources."
 }
 
@@ -59,23 +59,23 @@ variable "route_tags" {
 }
 
 variable "routing_mode" {
-  type = string
+  type        = string
   description = "The network-wide routing mode to use. If set to 'REGIONAL', this network's cloud routers will only advertise routes with subnetworks of this network in the same region as the router. If set to 'GLOBAL', this network's cloud routers will advertise routes with all subnetworks of this network, across regions. Possible values: [\"REGIONAL\", \"GLOBAL\"]"
 }
 
 variable "route_priority" {
-  type = number
+  type        = number
   description = "The priority of this route. Priority is used to break ties in cases where there is more than one matching route of equal prefix length. In the case of two routes with equal prefix length, the one with the lowest-numbered priority value wins. Default value is 1000. Valid range is 0 through 65535."
 }
 
 variable "next_hop_gateway" {
   description = "URL to a gateway that should handle matching packets."
-  type = string
+  type        = string
 }
 
 variable "destination_range" {
   description = "The destination range of outgoing packets that this route applies to. Only IPv4 is supported."
-  type = string
+  type        = string
 }
 
 variable "credentials_file_path" {
