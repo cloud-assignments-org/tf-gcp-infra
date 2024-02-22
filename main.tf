7d9eea3c7751bbeccef5954e90e0222a27196359
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-02-21 17:52:01 -0800

    adding delete default routes

--- main.tf
+++ main.tf
@@ -14,10 +14,11 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc" {
-  name                    = var.vpc_name
-  auto_create_subnetworks = var.auto_create_subnets
-  routing_mode            = var.routing_mode
-  description             = var.vpc_description
+  name                            = var.vpc_name
+  auto_create_subnetworks         = var.auto_create_subnets
+  delete_default_routes_on_create = var.delete_default_routes
+  routing_mode                    = var.routing_mode
+  description                     = var.vpc_description
 }
 
 resource "google_compute_subnetwork" "webapp" {
