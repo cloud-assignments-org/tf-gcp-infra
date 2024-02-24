fcd998656c10091dfdf331884b8cc7d1ce7f348e
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-02-24 17:43:35 -0500

    Variablizing stuff in the private service file

--- private.service.variables.tf
+++ private.service.variables.tf
@@ -13,3 +13,14 @@ variable "private_service_address_type" {
   type        = string
   description = "The type of the address to reserve: EXTERNAL indicates public/external single IP address., INTERNAL indicates internal IP ranges belonging to some network. Default value: \"EXTERNAL\" Possible values: [\"EXTERNAL\", \"INTERNAL\"]"
 }
+
+variable "private_service_prefix_length" {
+  description = "The prefix length for the IP range of the private service."
+  type        = number
+  default     = 16
+}
+variable "private_connection_service" {
+  description = "The service to connect via the private connection."
+  type        = string
+  default     = "servicenetworking.googleapis.com"
+}
