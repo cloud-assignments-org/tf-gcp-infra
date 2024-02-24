802dfb92cbf7a092a93c41a773357b6601862e1a
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-02-23 22:17:33 -0500

    using variable for firewall direction

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "google" {
-  project     = var.project_id
-  region      = var.region
+  project = var.project_id
+  region  = var.region
 }
 
 resource "google_compute_network" "vpc" {
