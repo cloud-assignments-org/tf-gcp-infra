3aecc49e934958f8cab0018f73a9ededae6addcf
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-02-24 17:43:35 -0500

    Variablizing stuff in the private service file

--- private.service.tf
+++ private.service.tf
@@ -1,20 +1,18 @@
 # Reference article https://cloud.google.com/sql/docs/mysql/configure-private-services-access#create_a_private_connection
 
-# TODO: Add variables before committing this file
-
 
 # Allocate an IP address range
 resource "google_compute_global_address" "private_ip_address" {
   name          = var.private_service_name
   purpose       = var.private_service_purpose
-  address_type  = "INTERNAL"
-  prefix_length = 16
+  address_type  = var.private_service_address_type
+  prefix_length = var.private_service_prefix_length
   network       = google_compute_network.vpc.self_link
 }
 
 # Create a private connection
 resource "google_service_networking_connection" "default" {
   network                 = google_compute_network.vpc.self_link
-  service                 = "servicenetworking.googleapis.com"
+  service                 = var.private_connection_service
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
