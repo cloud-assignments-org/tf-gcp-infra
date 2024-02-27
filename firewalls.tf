1226973021d1b4ab4e831ccf264428e8214a1e24
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-02-27 11:33:58 -0500

    added deny all firewall

--- firewalls.tf
+++ firewalls.tf
@@ -3,6 +3,7 @@
 resource "google_compute_firewall" "allow_app_traffic" {
   name    = "${var.vpc_name}-allow-app-traffic"
   network = google_compute_network.vpc.self_link
+  priority = var.allow_app_traffic_priority
 
   allow {
     protocol = var.application_protocol
@@ -17,10 +18,37 @@ resource "google_compute_firewall" "deny_ssh" {
   name      = "${var.vpc_name}-deny-ssh"
   network   = google_compute_network.vpc.self_link
   direction = var.direction_ingress
+  priority  = var.deny_ssh_priority
   deny {
     protocol = var.ssh_protocol
     ports    = var.ssh_ports
   }
 
   source_ranges = var.source_ranges_internet
+}
+
+resource "google_compute_firewall" "deny_all_tcp" {
+  name      = "${var.vpc_name}-deny-all-tcp"
+  network   = google_compute_network.vpc.self_link
+  direction = var.direction_ingress
+  priority  = var.deny_all_priority
+  deny {
+    protocol = var.tcp_protocol
+  }
+
+  // currently 0.0.0.0/0 implies all IPv4 addresses
+  source_ranges = var.source_ranges_internet 
+}
+
+resource "google_compute_firewall" "deny_all_udp" {
+  name      = "${var.vpc_name}-deny-all-udp"
+  network   = google_compute_network.vpc.self_link
+  direction = var.direction_ingress
+  priority  = var.deny_all_priority
+  deny {
+    protocol = var.udp_protocol
+  }
+
+  // currently 0.0.0.0/0 implies all IPv4 addresses
+  source_ranges = var.source_ranges_internet 
 }
