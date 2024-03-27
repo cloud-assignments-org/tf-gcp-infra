63f3a2c9ab28d98f52b8178a0365a93eddc7b903
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-03-27 02:00:42 -0400

    Variablized serverless connnector

---
+++ connectors.variables.tf
@@ -0,0 +1,9 @@
+variable "serverless_connector_name" {
+  type = string
+  description = "Name of the serverless vpc connector"
+}
+
+variable "serverless_connector_cidr" {
+  type = string
+  description = "CIDR range of the serverless vpc connector"
+}
