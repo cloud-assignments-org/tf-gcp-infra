2c6f28d45cd9a380bc74a7a12b24487119bed60d
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-03-27 13:23:27 -0400

    setting topic message retention duration

--- pubsub.topic.variables.tf
+++ pubsub.topic.variables.tf
@@ -3,3 +3,7 @@ variable "pub_sub_topic_name" {
   description = "Name of the topic"
 }
 
+variable "message_retention_duration" {
+  type        = string
+  description = " Indicates the minimum duration to retain a message after it is published to the topic. If this field is set, messages published to the topic in the last messageRetentionDuration are always available to subscribers. For instance, it allows any attached subscription to seek to a timestamp that is up to messageRetentionDuration in the past. If this field is not set, message retention is controlled by settings on individual subscriptions. The rotation period has the format of a decimal number, followed by the letter s (seconds). Cannot be more than 31 days or less than 10 minutes."
+}
