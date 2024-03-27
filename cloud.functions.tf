65e66a99129314ea13e5ef3c93f305ff91153b74
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-03-27 09:44:23 -0400

    converting cloud functions to variables

--- cloud.functions.tf
+++ cloud.functions.tf
@@ -1,27 +1,27 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloudfunctions2_function
 data "google_storage_bucket_object" "cloud_fn_source" {
-  name   = "trigger-user-verification-email.zip"
-  bucket = "cloud-dev-project-417513-bucket"
+  name   = var.cloud_fn_source_name
+  bucket = var.cloud_fn_source_bucket
 }
 
 resource "random_string" "cloud_fn_random_suffix" {
-  length           = 5
-  special          = false
-  override_special = "/@£$"
+  length           = var.cloud_fn_random_suffix_len
+  special          = var.cloud_fn_random_suffix_spl
+  override_special = var.cloud_fn_random_suffix_ovRide_special
 }
 
 resource "google_cloudfunctions2_function" "trigger_user_verification_email_fn" {
-  name        = "trigger-user-verificaiton-email-${random_string.cloud_fn_random_suffix.result}"
+  name        = "${var.trigger_user_verification_email_fn_prefix}-${random_string.cloud_fn_random_suffix.result}"
   location    = var.region
-  description = "This function triggers an email to users to have their user name validated"
+  description = var.trigger_user_verification_email_fn_description
 
-  depends_on = [data.google_storage_bucket_object.cloud_fn_source, 
-  google_vpc_access_connector.connector
+  depends_on = [data.google_storage_bucket_object.cloud_fn_source,
+    google_vpc_access_connector.connector
   ]
 
   build_config {
-    runtime     = "nodejs20"
-    entry_point = "triggerUserVerificationEmail" # Set the entry point 
+    runtime     = var.build_config_runtime
+    entry_point = var.build_config_entrypoint # Set the entry point 
     source {
       storage_source {
         bucket = data.google_storage_bucket_object.cloud_fn_source.bucket
@@ -31,37 +31,37 @@ resource "google_cloudfunctions2_function" "trigger_user_verification_email_fn" 
   }
 
   service_config {
-    max_instance_count               = 1
-    min_instance_count               = 0
-    available_memory                 = "256Mi"
-    timeout_seconds                  = 60
-    max_instance_request_concurrency = 1
-    available_cpu                    = "0.167"
+    max_instance_count               = var.fn_max_instance_count
+    min_instance_count               = var.fn_min_instance_count
+    available_memory                 = var.fn_available_memory
+    timeout_seconds                  = var.fn_timeout_sec
+    max_instance_request_concurrency = var.fn_concurrent_requests
+    available_cpu                    = var.fn_available_cpu_str
     environment_variables = {
-      PROTOCOL               = "http"
-      DOMAIN                 = "parthadhruv.com"
-      API_PORT               = "3000"
-      VERSION                = "v1"
-      VERIFY_END_POINT       = "user/verifyEmail"
-      SET_VALIDITY_END_POINT = "user/setValidity"
-      VALIDITY_MINUTES       = "2"
-      MAILCHIMP_API_KEY      = "md-yz6F3wdSdOFqPdtTPVQFLQ"
-      DATABASE_USER     = "${google_sql_user.user.name}"
-      DATABASE_PASSWORD = "${google_sql_user.user.password}"
-      DATABASE_IP       = "${google_sql_database_instance.instance.private_ip_address}"
-      DB_PORT           = "${var.db_port}"
-      DB_NAME           = "${google_sql_database.database.name}"
+      PROTOCOL               = var.env_variable_protocol
+      DOMAIN                 = var.env_variable_domain
+      API_PORT               = var.env_variables_api_port
+      VERSION                = var.env_variables_api_version
+      VERIFY_END_POINT       = var.env_variables_verify_end_point
+      SET_VALIDITY_END_POINT = var.env_variables_set_validity_end_point
+      VALIDITY_MINUTES       = var.env_variables_validity_minutes
+      MAILCHIMP_API_KEY      = var.email_server_API_KEY
+      DATABASE_USER          = google_sql_user.user.name
+      DATABASE_PASSWORD      = google_sql_user.user.password
+      DATABASE_IP            = google_sql_database_instance.instance.private_ip_address
+      DB_PORT                = var.db_port
+      DB_NAME                = google_sql_database.database.name
 
     }
-    vpc_connector = google_vpc_access_connector.connector.self_link
-    vpc_connector_egress_settings = "PRIVATE_RANGES_ONLY"
+    vpc_connector                 = google_vpc_access_connector.connector.self_link
+    vpc_connector_egress_settings = var.vpc_connector_egress_settings
 
-    ingress_settings               = "ALLOW_INTERNAL_ONLY"
+    ingress_settings               = var.service_ingress_setting
     all_traffic_on_latest_revision = true
   }
 
   event_trigger {
-    event_type            = "google.cloud.pubsub.topic.v1.messagePublished"
+    event_type            = var.event_type
     pubsub_topic          = google_pubsub_topic.user-created.id
     service_account_email = google_service_account.pub_sub_service_account.email
   }
---
+++ cloud.functions.variables.tf
@@ -0,0 +1,117 @@
+# Function Source
+variable "cloud_fn_source_name" {
+  type = string
+}
+
+variable "cloud_fn_source_bucket" {
+  type = string
+}
+
+# Function Name
+variable "cloud_fn_random_suffix_len" {
+  type = number
+}
+
+variable "cloud_fn_random_suffix_spl" {
+  type = bool
+}
+
+variable "cloud_fn_random_suffix_ovRide_special" {
+  type = string
+}
+
+# Function Definition
+variable "trigger_user_verification_email_fn_prefix" {
+  type = string
+}
+
+variable "trigger_user_verification_email_fn_description" {
+  type = string
+}
+
+variable "build_config_runtime" {
+  type = string
+}
+
+variable "build_config_entrypoint" {
+  type = string
+}
+
+# Function service config
+
+
+
+
+
+variable "fn_max_instance_count" {
+  type = number
+}
+
+variable "fn_min_instance_count" {
+  type = number
+}
+
+variable "fn_available_memory" {
+  type = string
+}
+
+variable "fn_timeout_sec" {
+  type = number
+}
+
+variable "fn_concurrent_requests" {
+  type = number
+}
+
+variable "fn_available_cpu_str" {
+  type = string
+}
+
+# Environment Variables
+
+variable "env_variable_protocol" {
+  type = string
+}
+
+variable "env_variable_domain" {
+  type = string
+}
+
+variable "env_variables_api_port" {
+  type = string
+}
+
+
+variable "env_variables_api_version" {
+  type = string
+}
+
+variable "env_variables_verify_end_point" {
+  type = string
+}
+
+variable "env_variables_set_validity_end_point" {
+  type = string
+}
+
+variable "env_variables_validity_minutes" {
+  type = string
+}
+
+variable "email_server_API_KEY" {
+  type = string
+}
+
+# Function triggers ingress and egress 
+
+variable "vpc_connector_egress_settings" {
+  type = string
+}
+
+variable "service_ingress_setting" {
+  type = string
+}
+
+variable "event_type" {
+  type = string
+}
