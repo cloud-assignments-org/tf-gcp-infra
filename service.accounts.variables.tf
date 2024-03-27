032ce882dd6b805ac095eed6715c9641eb2b421b
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-03-27 09:54:48 -0400

    Updating service accounts to variables

--- service.accounts.variables.tf
+++ service.accounts.variables.tf
@@ -13,3 +13,14 @@ variable "compute_instance_service_acc_scopes" {
   type        = list(string)
   description = "(Required) A list of service scopes. Both OAuth2 URLs and gcloud short names are supported. To allow full access to all Cloud APIs, use the cloud-platform scope. "
 }
+
+
+variable "pub_sub_service_account_id" {
+  type    = string
+  default = "pub-sub-service-account"
+}
+
+variable "pub_sub_service_account_disp_name" {
+  type    = string
+  default = "Pub Sub Service Account"
+}
