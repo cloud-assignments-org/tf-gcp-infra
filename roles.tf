6d39b162377f029a4700857a2a2109bc766a1693
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-03-26 22:11:31 -0400

    Fixing roles of the pub sub event triggerer

--- roles.tf
+++ roles.tf
@@ -32,10 +32,28 @@ resource "google_project_iam_binding" "pub_sub_publisher" {
   ]
 }
 
+# We need to provide both the toke creator and the cloud functions invoker role 
+# to the pubsub service account
+# This service account will be used to trigger the cloud function
 resource "google_project_iam_binding" "service_acc_token_creator" {
   project = var.project_id
   role    = "roles/iam.serviceAccountTokenCreator"
 
+  members = [
+    "serviceAccount:${google_service_account.pub_sub_service_account.email}"
+  ]
+}
+
+
+# https://cloud.google.com/run/docs/troubleshooting#unauthorized-client
+# we are facing the following issue: 
+# textPayload: "The request was not authenticated. Either allow unauthenticated invocations or set the proper Authorization header. Read more at https://cloud.google.com/run/docs/securing/authenticating Additional troubleshooting documentation can be found at: https://cloud.google.com/run/docs/troubleshooting#unauthorized-client"
+
+# we need to provide the following role instead of the cloudfunctions.invoker role
+# roles/run.invoker
+resource "google_project_iam_binding" "cloud_functions_invoker" {
+  project = var.project_id
+  role    = "roles/run.invoker"
   members = [
     "serviceAccount:${google_service_account.pub_sub_service_account.email}"
   ]
