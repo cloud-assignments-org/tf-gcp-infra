d9091551a1b78ab838e2bdbc014d50246555f1d6
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-03-27 13:23:27 -0400

    setting topic message retention duration

--- pubsub.topic.tf
+++ pubsub.topic.tf
@@ -1,3 +1,4 @@
 resource "google_pubsub_topic" "user-created" {
-  name = var.pub_sub_topic_name
+  name                       = var.pub_sub_topic_name
+  message_retention_duration = var.message_retention_duration
 }
