082e6a3fdada133dcf84bce85e27e1fa5ba6f04b
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-04-02 14:01:04 -0400

    variablizing health check firewalls

--- firewalls.variables.tf
+++ firewalls.variables.tf
@@ -67,3 +67,69 @@ variable "udp_protocol" {
   description = "Refers to the udp protocol"
   default     = "udp"
 }
+
+# HEALTH CHECK FIREWALL
+variable "health_check_firewall_name" {
+  type        = string
+  description = "The name of the health check firewall"
+}
+
+variable "health_check_firewall_priority" {
+  type        = number
+  description = "The priority of the health check firewall rule"
+}
+
+variable "health_check_firewall_source_ranges" {
+  type        = list(string)
+  description = "The source IP ranges that are allowed to access the health check firewall"
+  default     = ["130.211.0.0/22", "35.191.0.0/16"]
+}
+
+variable "health_check_firewall_target_tags" {
+  type        = list(string)
+  description = "A list of instance tags for the health check firewall, indicating the set of instances in the network that the firewall policy applies to"
+}
+
+variable "health_check_firewall_protocol" {
+  type        = string
+  description = "The protocol used by the health check firewall rule"
+}
+
+variable "health_check_firewall_direction" {
+  type        = string
+  description = "The direction of traffic to which the health check firewall rule applies"
+}
+
+
+# Load Balancer Proxy
+variable "allow_lb_proxy_name" {
+  type        = string
+  description = "The name of the load balancer proxy firewall"
+}
+
+variable "allow_lb_proxy_priority" {
+  type        = number
+  description = "The priority of the load balancer proxy firewall rule"
+  default     = 100
+}
+
+variable "allow_lb_proxy_source_ranges" {
+  type        = list(string)
+  description = "The source IP ranges that are allowed to access the load balancer proxy firewall"
+}
+
+variable "allow_lb_proxy_target_tags" {
+  type        = list(string)
+  description = "A list of instance tags for the load balancer proxy firewall, indicating the set of instances in the network that the firewall policy applies to"
+}
+
+variable "allow_lb_proxy_protocol" {
+  type        = string
+  description = "The protocol used by the load balancer proxy firewall rule"
+}
+
+variable "allow_lb_proxy_direction" {
+  type        = string
+  description = "The direction of traffic to which the load balancer proxy firewall rule applies"
+  default     = "INGRESS"
+}
