b3a27505c098ba768f5ab999849e3372d71e7633
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-04-02 18:45:02 -0400

    adding variables to auto scaler

--- auto.scaler.variables.tf
+++ auto.scaler.variables.tf
@@ -33,3 +33,13 @@ variable "auto_scaler_mode" {
   description = "The operating mode for the autoscaler"
   default     = "ON"
 }
+
+variable "auto_scaler_scale_in_control_max_scaled_replica_percent" {
+  type        = number
+  description = "Specifies a percentage of instances between 0 to 100%, inclusive. For example, specify 80 for 80%."
+}
+
+variable "auto_scaler_scale_in_control_time_window_sec" {
+  type        = number
+  description = "How long back autoscaling should look when computing recommendations to include directives regarding slower scale down, as described above."
+}
