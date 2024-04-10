a48aafd7b641a01fb702f916b77e3cd37ae5b1bc
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-04-10 02:07:10 -0400

    Added google beta provider

--- main.tf
+++ main.tf
@@ -4,10 +4,19 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 5.17"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.24"
+    }
   }
 }
 
 provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "google-beta" {
   project = var.project_id
   region  = var.region
 }
