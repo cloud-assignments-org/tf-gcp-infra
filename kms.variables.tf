622442d05258d565f29c53ac373917cdaad9420c
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-04-10 13:54:56 -0400

    randomizing kms names

--- kms.variables.tf
+++ kms.variables.tf
@@ -25,3 +25,13 @@ variable "kms_storage_bucket_crypto_key_name" {
 variable "kms_storage_bucket_crypto_key_rotation_period" {
   type = string
 }
+
+variable "kms_key_names_random_string_length" {
+  type = number
+  default = 5
+}
+
+variable "kms_key_names_random_string_suffix_spl" {
+  type = bool
+  default = false
+}
